b60669d7aac56dcafec06b93994f2cb61bef0a75
Gabriel Nattis <gabrielbits@live.com>
2021-03-17 17:20:27 -0300

    update: removed grunt files/config, updated protractor.conf.js, added concurrently (not working atm), added testConf script

--- e2e/specs/todo-spec.js
+++ e2e/specs/todo-spec.js
@@ -1,18 +1,12 @@
-describe('angularjs homepage todo list', function() {
-    it('should add a todo', function() {
-      browser.get('https://angularjs.org');
-  
-      element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-      element(by.css('[value="add"]')).click();
-  
-      var todoList = element.all(by.repeater('todo in todoList.todos'));
-      expect(todoList.count()).toEqual(3);
-      expect(todoList.get(2).getText()).toEqual('write first protractor test');
-  
-      // You wrote your first test, cross it off the list
-      todoList.get(2).element(by.css('input')).click();
-      var completedAmount = element.all(by.css('.done-true'));
-      expect(completedAmount.count()).toEqual(2);
+const { element, by, browser } = require("protractor");
+
+describe('Amazon buying page', function() {
+    it('should find an available PS5', function() {
+      //browser.get('https://www.amazon.com.br/dp/B088GNRX3J/ref=s9_acss_bw_cg_HeroVG_1a1_w?pf_rd_m=A3RN7G7QC5MWSZ&pf_rd_s=merchandised-search-5&pf_rd_r=HXYVSMZGR259EQ0NZKWF&pf_rd_t=101&pf_rd_p=77a4af07-b03d-498f-9c59-51c9513d27bc&pf_rd_i=21362266011');
+      browser.waitForAngularEnabled(false);
+      browser.get('https://www.amazon.com.br/Tomie-1-2-Junji-Ito/dp/6586672376/ref=pd_rhf_ee_s_pd_crcd_2?pd_rd_w=Hc20g&pf_rd_p=b762b69e-2f69-438e-86c7-b1c583041006&pf_rd_r=6Z40V9K0ANFKSQ7KVH88&pd_rd_r=61b94971-1c0e-46cc-ac62-320db3a797e7&pd_rd_wg=wrY2K&pd_rd_i=6586672376&psc=1');
+
+      var buyButton = element(by.id('add-to-cart-button'));
+      expect(buyButton.isDisplayed()).toBe(true);
     });
   });
-  
